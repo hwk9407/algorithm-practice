a015462379b5d58fe8251717bd49ddf8c3ef60ed
hwk9407 <hwk__@naver.com>
2025-02-09 16:32:29 +0900

    [876]Middle of the Linked List

--- src/leetcode/java/Solution876/Solution.java
+++ src/leetcode/java/Solution876/Solution.java
@@ -19,20 +19,15 @@ import java.util.List;
 class Solution {
 
     public ListNode middleNode(ListNode head) {
-        int nodeSize = 0;
-        ListNode current = head;
+        ListNode slow = head;
+        ListNode fast = head;
 
-        while (current != null) {
-            nodeSize++;
-            current = current.next;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
         }
 
-        current = head;
-        for (int i = 0; i < nodeSize / 2; i++) {
-            current = current.next;
-        }
-
-        return current;
+        return slow;
     }
 
     public static void main(String[] args) {
