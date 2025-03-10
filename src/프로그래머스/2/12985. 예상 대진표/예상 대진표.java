5192a0a3cf1561cac28b980f6e1760f2dae0b6fb
hwk9407 <hwk__@naver.com>
2025-03-10 09:36:02 +0900

    [level 2] Title: 예상 대진표, Time: 0.05 ms, Memory: 70.3 MB -BaekjoonHub

--- src/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
+++ src/프로그래머스/2/12985. 예상 대진표/예상 대진표.java
@@ -2,13 +2,6 @@ class Solution
 {
     public int solution(int n, int a, int b)
     {
-        int answer = 0;
-        while (a != b) {
-            answer++;
-            a = (a + 1) / 2;
-            b = (b + 1) / 2;
-        }
-        
-        return answer;
+        return Integer.toBinaryString((a - 1) ^ (b - 1)).length();
     }
 }
