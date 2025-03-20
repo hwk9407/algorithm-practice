98dce28d09834204acf5a810994190a94e5210ea
hwk9407 <hwk__@naver.com>
2025-03-21 07:54:02 +0900

    [level 2] Title: 피로도, Time: 3.99 ms, Memory: 71.8 MB -BaekjoonHub

--- src/프로그래머스/2/87946. 피로도/피로도.java
+++ src/프로그래머스/2/87946. 피로도/피로도.java
@@ -2,41 +2,21 @@ import java.util.Stack;
 
 class Solution {
     public int solution(int k, int[][] dungeons) {
-        // dungeons의 첫번째 인덱스의 갯수는 1 ~ 8
-        // dungeons[최소 필요 피로도, 소모 피로도]
-        // 던전에 입장하기 위한 최소 필요 피로도
-        // 던전 탐험 후 소모되는 소모 피로도
-        // k = 현재 피로도
-        // answer = 최대 탐험 가능한 던전 횟수
-        int answer = 0;
-        Stack<Integer> maxCount = new Stack<>();
-        Stack<Integer> stackK = new Stack<>();
-        Stack<boolean[]> visited = new Stack<>();
-        
-        maxCount.push(answer);
-        stackK.push(k);
-        visited.push(new boolean[dungeons.length]);
-        
-        while (!stackK.isEmpty()) {
-            int current = stackK.pop();
-            int count = maxCount.pop();
-            boolean[] visitFlag = visited.pop();
-            
-            answer = Math.max(count, answer);
-            
-            for (int i = 0; i < dungeons.length; i++) {
-                if (!visitFlag[i] && current >= dungeons[i][0]) {
-                    visitFlag[i] = true;
-                    
-                    maxCount.push(count + 1);
-                    stackK.push(current - dungeons[i][1]);
-                    visited.push(visitFlag.clone());
-                    
-                    visitFlag[i] = false;
-                }
-            }
-        }
+        int answer = dfs(k, 0, dungeons, new boolean[dungeons.length]);
         
         return answer;
+    }
+    
+    private int dfs(int k, int count, int[][] dungeons, boolean[] visited) {
+        int maxCount = count;
+        
+        for (int i = 0; i < dungeons.length; i++) {
+            if (!visited[i] && k >= dungeons[i][0]) {
+                visited[i] = true;
+                maxCount = Math.max(maxCount, dfs(k - dungeons[i][1], count + 1, dungeons, visited));
+                visited[i] = false;
+            }
+        }
+        return maxCount;
     }
 }
