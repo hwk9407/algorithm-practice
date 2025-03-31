7b8dacf946bd437141910dd2b612e3e79cd4bb20
hwk9407 <hwk__@naver.com>
2025-03-31 12:48:49 +0900

    숫자 조합으로 타겟 만들기2

--- src/exercisebook/CombinationTargetNumber.java
+++ src/exercisebook/CombinationTargetNumber.java
@@ -1,28 +1,27 @@
 package exercisebook;
 
 /**
- * 정수 배열 nums가 주어지고,
- * 각 요소마다 + 또는 - 연산을 붙일 수 있다.
- * 그렇게 해서 나온 모든 조합 중,
- * 총합이 짝수인 경우의 수를 구하라.
+ * 정수 배열 nums가 주어진다.
+ * 이 숫자들 사이에 연산자 +, -, * 중 하나를 넣어 수식을 만든다.
+ * 연산자 우선순위 없이 왼쪽부터 순차적으로 계산하며,
+ * 그렇게 만든 식의 결과가 target과 같은 경우의 수를 구하라.
  */
 public class CombinationTargetNumber {
 
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3};
-
-        int result = evenCount(nums, 0, 0);
+        int[] nums = {2, 1, 1};
+        int target = 4;
+        int result = dfs(nums, 1, nums[0], target);
         System.out.println("result = " + result);
     }
 
-    private static int evenCount(int[] nums, int idx, int sum) {
-        if (idx == nums.length) {
-            return sum % 2 == 0 ? 1 : 0;
-        }
+    private static int dfs(int[] nums, int idx, int currentResult, int target) {
+        if (idx == nums.length) return currentResult == target ? 1 : 0;
 
-        int plus = evenCount(nums, idx + 1, sum + nums[idx]);
-        int minus = evenCount(nums, idx + 1, sum - nums[idx]);
+        int plus = dfs(nums, idx + 1, currentResult + nums[idx], target);
+        int minus = dfs(nums, idx + 1, currentResult - nums[idx], target);
+        int multiple = dfs(nums, idx + 1, currentResult * nums[idx], target);
 
-        return plus + minus;
+        return plus + minus + multiple;
     }
 }
