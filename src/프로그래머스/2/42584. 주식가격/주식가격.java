785646a6162d631a0c7c40c64b2e302d127165af
hwk9407 <hwk__@naver.com>
2025-04-04 23:38:53 +0900

    [level 2] Title: 주식가격, Time: 35.68 ms, Memory: 76.7 MB -BaekjoonHub

---
+++ src/프로그래머스/2/42584. 주식가격/주식가격.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] prices) {
+        int[] answer = new int[prices.length];
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < prices.length; i++) {
+            answer[i] = prices.length - 1 - i;
+            while (!stack.isEmpty() && prices[stack.peek()] > prices[i]) {
+                int idx = stack.pop();
+                answer[idx] = i - idx;
+            }
+            stack.push(i);
+        }
+        
+        return answer;
+    }
+}
