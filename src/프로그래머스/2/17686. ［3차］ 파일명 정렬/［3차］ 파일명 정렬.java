f48fe5b052cacc93b13bf286c5a5d3cf51642313
hwk9407 <hwk__@naver.com>
2025-04-11 23:10:04 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 15.82 ms, Memory: 92.7 MB -BaekjoonHub

---
+++ src/프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String[] files) {
+        File[] fileArr = new File[files.length];
+        for (int i = 0; i < files.length; i++) {
+            fileArr[i] = new File(files[i], i);
+        }
+        Arrays.sort(fileArr);
+
+        String[] answer = Arrays.stream(fileArr).map(file -> file.name).toArray(String[]::new);
+        return answer;
+    }
+    
+    class File implements Comparable<File> {
+        String name;
+        int index;
+        String head;
+        int number;
+
+        public File(String file, int index) {
+            int i = 0;
+            while (i < file.length() && !Character.isDigit(file.charAt(i))) {
+                i++;
+            }
+            int j = i;
+            while (j < file.length() && Character.isDigit(file.charAt(j))) {
+                j++;
+            }
+
+            this.name = file;
+            this.index = index;
+            this.head = file.substring(0, i).toLowerCase();
+            this.number = Integer.parseInt(file.substring(i, j));
+        }
+
+        @Override
+        public int compareTo(File o) {
+            if (!this.head.equals(o.head)) return this.head.compareTo(o.head);
+            if (this.number != o.number) return this.number > o.number ? 1 : -1;
+            return this.index > o.index ? 1 : -1;
+        }
+    }
+}
