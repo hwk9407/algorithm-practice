cdf55462d164eaa86246d79ccb7649fd446397bb
hwk9407 <hwk__@naver.com>
2025-04-25 00:44:39 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 7.77 ms, Memory: 54.9 MB -BaekjoonHub

--- src/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ src/프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,34 @@
-import java.util.LinkedList;
 import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
     public int solution(int[][] maps) {
-        int n = maps.length;
-        int m = maps[0].length;
-        boolean[][] visited = new boolean[n][m];
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0, 0, 1});
-        visited[0][0] = true;
+        int[] dx = {0, 1, 0, -1}; // 남 동 북 서
+        int[] dy = {1, 0, -1, 0};
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0, 0, 1});
+        maps[0][0] = 0;
+        
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int move = current[2];
+            
+            if (x == maps[0].length - 1 && y == maps.length - 1) return move;
+            
+            for (int dir = 0; dir < 4; dir++) {
+                int nx = x + dx[dir];
+                int ny = y + dy[dir];
 
-        int[] dx = {-1, 1, 0, 0};
-        int[] dy = {0, 0, -1, 1};
-
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int x = cur[0], y = cur[1], dist = cur[2];
-
-            if (x == n - 1 && y == m - 1) return dist;
-
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-
-                if (canMove(nx, ny, maps, visited)) {
-                    visited[nx][ny] = true;
-                    q.add(new int[]{nx, ny, dist + 1});
+                if (nx < 0 || ny < 0 || nx >= maps[0].length || ny >= maps.length || maps[ny][nx] == 0) {
+                    continue;
                 }
+                
+                maps[ny][nx] = 0;
+                queue.add(new int[]{nx, ny, move + 1});
             }
         }
         return -1;
-    }
-    
-    private boolean canMove(int x, int y, int[][] maps, boolean[][] visited) {
-        return x >= 0 && y >= 0 && x < maps.length && y < maps[0].length && maps[x][y] == 1 && !visited[x][y];
     }
 }
