9aa314a1f75a625be0cbd977c7f2877359c67fdb
hwk9407 <hwk__@naver.com>
2025-05-03 21:37:31 +0900

    [level 2] Title: 124 나라의 숫자, Time: 0.14 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ src/프로그래머스/2/12899. 124 나라의 숫자/124 나라의 숫자.java
@@ -0,0 +1,19 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        
+        while (n > 0) {
+            int rest = n % 3;
+            if (rest == 0) {
+                sb.insert(0, "4");
+                n = n / 3 - 1;
+            } else {
+                sb.insert(0, Integer.toString(rest));
+                n /= 3;
+            }
+        }
+        
+        String answer = sb.toString();
+        return answer;
+    }
+}
