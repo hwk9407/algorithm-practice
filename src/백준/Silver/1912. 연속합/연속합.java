321d24af233c3c0a990ea725f5e959d0e5cb6d04
hwk9407 <wonhur5@gmail.com>
2025-08-01 06:35:30 +0900

    [Silver II] Title: 연속합, Time: 236 ms, Memory: 25472 KB -BaekjoonHub

---
+++ src/백준/Silver/1912. 연속합/연속합.java
@@ -0,0 +1,23 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        String[] split = br.readLine().split(" ");
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(split[i]);
+        }
+
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        int max = arr[0];
+
+        for (int i = 1; i < n; i++) {
+            dp[i] = Math.max(arr[i], dp[i - 1] + arr[i]);
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
