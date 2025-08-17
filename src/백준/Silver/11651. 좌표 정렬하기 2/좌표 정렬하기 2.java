abe05739f8c54b9561b19edc4f54c2cd7a5ef560
hwk9407 <wonhur5@gmail.com>
2025-08-17 18:45:00 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 704 ms, Memory: 64164 KB -BaekjoonHub

---
+++ src/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    private static class Point implements Comparable<Point> {
+        int x;
+        int y;
+
+        Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public int compareTo(Point p) {
+            if (this.y != p.y) return this.y - p.y;
+            return this.x - p.x;
+        }
+
+        @Override
+        public String toString() {
+            return this.x + " " + this.y;
+        }
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        Set<Point> set = new TreeSet<>();
+        for (int i = 0; i < n; i++) {
+            String[] line = br.readLine().split(" ");
+            set.add(new Point(Integer.parseInt(line[0]), Integer.parseInt(line[1])));
+        }
+        StringBuilder sb = new StringBuilder();
+        for (Point p : set) {
+            sb.append(p).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
