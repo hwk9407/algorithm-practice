9ea37440a6088908b4c0d0355b3e8acd5aea30c0
hwk9407 <wonhur5@gmail.com>
2025-09-14 23:57:46 +0900

    [Gold V] Title: 회의실 배정, Time: 484 ms, Memory: 44444 KB -BaekjoonHub

---
+++ src/백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] arr = new int[n][2];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            arr[i][0] = Integer.parseInt(st.nextToken());
+            arr[i][1] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr, (a, b) -> {
+            if (a[1] != b[1]) return Integer.compare(a[1], b[1]);
+            return Integer.compare(a[0], b[0]);
+        });
+
+        int count = 0;
+        int last = 0;
+        for (int i = 0; i < n; i++) {
+            if (arr[i][0] < last) continue;
+            count++;
+            last = arr[i][1];
+        }
+        System.out.println(count);
+    }
+}
