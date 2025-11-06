cfcd789438038e5a73ecfb5bf170c37dc7db90d5
hwk9407 <wonhur5@gmail.com>
2025-11-06 17:27:46 +0900

    [Gold IV] Title: 플로이드, Time: 380 ms, Memory: 42444 KB -BaekjoonHub

---
+++ src/백준/Gold/11404. 플로이드/플로이드.java
@@ -0,0 +1,43 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+
+        int[][] dist = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) Arrays.fill(dist[i], Integer.MAX_VALUE);
+        for (int i = 1; i <= n; i++) dist[i][i] = 0;
+
+        StringTokenizer st;
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            dist[a][b] = Math.min(dist[a][b], c);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] != Integer.MAX_VALUE && dist[k][j] != Integer.MAX_VALUE) {
+                        dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] == Integer.MAX_VALUE) dist[i][j] = 0;
+                sb.append(dist[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
+}
