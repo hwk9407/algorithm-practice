4346d637991836e5949ccfb17a983672418ac9cf
hwk9407 <wonhur5@gmail.com>
2025-11-12 18:57:03 +0900

    [Gold IV] Title: 서강그라운드, Time: 116 ms, Memory: 14392 KB -BaekjoonHub

---
+++ src/백준/Gold/14938. 서강그라운드/서강그라운드.java
@@ -0,0 +1,57 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        int[] items = new int[n + 1];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 1; i <= n; i++) items[i] = Integer.parseInt(st.nextToken());
+
+        int[][] dist = new int[n + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == j) dist[i][j] = 0;
+                else dist[i][j] = Integer.MAX_VALUE;
+            }
+        }
+
+        for (int i = 0; i < r; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int l = Integer.parseInt(st.nextToken());
+            dist[a][b] = Math.min(dist[a][b], l);
+            dist[b][a] = Math.min(dist[b][a], l);
+        }
+
+        for (int k = 1; k <= n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if (dist[i][k] == Integer.MAX_VALUE || dist[k][j] == Integer.MAX_VALUE) continue;
+                    if (dist[i][j] > dist[i][k] + dist[k][j]) {
+                        dist[i][j] = dist[i][k] + dist[k][j];
+                    }
+                }
+            }
+        }
+
+        int answer = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= n; j++) {
+                if (dist[i][j] <= m) {
+                    sum += items[j];
+                }
+            }
+            answer = Math.max(answer, sum);
+        }
+
+        System.out.println(answer);
+    }
+}
