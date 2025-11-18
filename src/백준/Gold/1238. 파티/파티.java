46d1ed564ef699c8d4e246293371fc27e55b9bce
hwk9407 <wonhur5@gmail.com>
2025-11-19 01:05:05 +0900

    [Gold III] Title: 파티, Time: 200 ms, Memory: 19628 KB -BaekjoonHub

--- src/백준/Gold/1238. 파티/파티.java
+++ src/백준/Gold/1238. 파티/파티.java
@@ -25,39 +25,32 @@ public class Main {
             reverseGraph.get(end).add(new int[]{start, t});
         }
 
-        int result = -1;
-        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-        int[] distToX = new int[n + 1];
-        Arrays.fill(distToX, -1);
-        pq.offer(new int[]{x, 0});
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            if (distToX[current[0]] != -1) continue;
-            distToX[current[0]] = current[1];
-            for (int[] next : reverseGraph.get(current[0])) {
-                if (distToX[next[0]] != -1) continue;
-                pq.offer(new int[]{next[0], current[1] + next[1]});
-            }
-        }
-        int[] distFromX = new int[n + 1];
-        Arrays.fill(distFromX, -1);
-        pq.offer(new int[]{x, 0});
-        while (!pq.isEmpty()) {
-            int[] current = pq.poll();
-            if (distFromX[current[0]] != -1) continue;
-            distFromX[current[0]] = current[1];
-            for (int[] next : graph.get(current[0])) {
-                if (distFromX[next[0]] != -1) continue;
-                pq.offer(new int[]{next[0], current[1] + next[1]});
-            }
-        }
+        int[] distToX = dijkstra(reverseGraph, x, n);
+        int[] distFromX = dijkstra(graph, x, n);
 
-        distFromX[x] = 0;
-        distToX[x] = 0;
+        int result = 0;
         for (int i = 1; i <= n; i++) {
             result = Math.max(result, distFromX[i] + distToX[i]);
         }
 
+
         System.out.println(result);
+    }
+
+    private static int[] dijkstra(List<List<int[]>> graph, int x, int n) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
+        int[] result = new int[n + 1];
+        Arrays.fill(result, -1);
+        pq.offer(new int[]{x, 0});
+        while (!pq.isEmpty()) {
+            int[] current = pq.poll();
+            if (result[current[0]] != -1) continue;
+            result[current[0]] = current[1];
+            for (int[] next : graph.get(current[0])) {
+                if (result[next[0]] != -1) continue;
+                pq.offer(new int[]{next[0], current[1] + next[1]});
+            }
+        }
+        return result;
     }
 }
