36000dcc701e162e205aaec61166cfb9d4dfd60f
hwk9407 <wonhur5@gmail.com>
2025-12-14 23:07:00 +0900

    Time: 2 ms (92.22%), Space: 64.9 MB (60.53%) - LeetHub

---
+++ src/leetcode/0238-product-of-array-except-self/0238-product-of-array-except-self.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] productExceptSelf(int[] nums) {
+        int len = nums.length;
+        int[] left = new int[len];
+        int[] right = new int[len];
+
+        right[0] = 1;
+        for (int i = 1; i < len; i++) {
+            right[i] = right[i - 1] * nums[i - 1];
+        }
+        left[len - 1] = 1;
+        for (int i = len - 2; i >= 0; i--) {
+            left[i] = left[i + 1] * nums[i + 1];
+        }
+
+        int[] answer = new int[len];
+        for (int i = 0; i < len; i++) {
+            answer[i] = left[i] * right[i];
+        }
+        return answer;
+    }
+}
